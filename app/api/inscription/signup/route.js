3e8bf2da603f99feb8602e77f6d5c0bc9bd0c0c9
junior-boop <juniorseppo3@gmail.com>
2023-09-08 11:54:58 +0100

    test voire

--- app/api/inscription/signup/route.js
+++ app/api/inscription/signup/route.js
@@ -4,24 +4,3 @@ import { connectToDB } from "@/utils/database"
 export const GET = async () => {
     return new Response('je suis inscription')
 }
-
-export async function POST(request){
-    const {name, surname, mail, tel, pw, town, like, share } = await request.json()
-
-    const user = {
-        name, surname, mail, tel, pw, town, like, share
-    }
-
-    
-
-    try { 
-        await connectToDB()
-        const NewUser = new User({
-            name, surname, mail, tel, pw, town, like, share
-        })
-        await NewUser.save()
-        return new Response(JSON.stringify(user), { status: 201 })
-    } catch(error){
-        return new Response('Il y a une erreur', { status: 500 })
-    }
-}
